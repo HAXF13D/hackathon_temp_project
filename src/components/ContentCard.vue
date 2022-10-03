5e3f03db2260ae2c46fcb8a408790bd4b60b0378
HAXF13D <vovik0312@gmail.com>
2022-10-04 00:15:07 +0300

    Add notification

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -1,6 +1,24 @@
 <template>
+    <div class="modal fade" :id="event.id" tabindex="-1" data-bs-backdrop="static" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content test">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Записаться на {{ event.header.toLowerCase() }}?</h5>
+            </div>
+            <div class="modal-body">
+                <p class="default-text disabled" >Мероприятие пройдет: {{ event.date_time }} </p>
+                <p class="default-text disabled" >Награда за мероприятие: {{ event.award }} </p>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-success default-text" data-bs-dismiss="modal" @click="registerToEvent()">Записаться</button>
+                <button type="button" class="btn btn-danger default-text" data-bs-dismiss="modal" @click="closeModal()">Закрыть</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row justify-content-center mt-5">
-        <div class="col-11 col-sm-8 col-xl-9 px-4 news-post test">
+        <div class="col-11 col-sm-8 col-xl-9 px-4 news-post test cursor-pointer" @click="showEventSignUP">
             <div class="row mt-3">
                 <div class="col-12">
                 <h2 class="default-text disabled">{{event.header}}</h2>
@@ -18,27 +36,107 @@
             </div>
             <div v-if="event.award" class="row mt-3">
                 <div class="col-6 align-middle">
-                    <p class="text-start fw-bold align-top">Дата проведения: {{event.date_time}}</p>
+                    <p class="text-start fw-bold align-top default-text disabled">Дата проведения: {{event.date_time}}</p>
                 </div>
                 <div class="col-6 align-middle">
-                    <p class="text-end fw-bold align-top">Награда: {{event.award}}</p>
+                    <p class="text-end fw-bold align-top default-text disabled">Награда: {{event.award}}</p>
                 </div>
             </div>
         </div>
     </div>
+
+
 </template>
 
 <script>
+
+    import { useToast } from "vue-toastification";
+
     export default {
         name: "events",
+        setup(){
+            const toast = useToast();
+            return { toast }
+        },
+        data: () => ({
+            isModalOpen: false,
+        }),
         props: ['event'],
-        beforeMount(){
-            console.log(this.event.award, 123321);
+        methods: {
+            showEventSignUP(){
+                if (this.event.award){
+                    if (!this.isModalOpen) {
+                        let myModal = new bootstrap.Modal(document.getElementById(this.event.id), {
+                            keyboard: false,
+                            focus: true
+                        });
+                        myModal.show();
+                        this.isModalOpen = true;
+                    }
+                }
+            },
+            closeModal(){
+                let myModal = new bootstrap.Modal(document.getElementById(this.event.id), {
+                    keyboard: false,
+
+                });
+                myModal.hide();
+                this.isModalOpen = false;
+                
+            },
+            registerToEvent(){
+                console.log(1);
+                let myModal = new bootstrap.Modal(document.getElementById(this.event.id), {
+                    keyboard: false,
+
+                });
+                myModal.hide();
+                this.isModalOpen = false;
+                //this.toast.info("I'm an info toast!");
+                // Обработка записи
+                this.toast.success("Запись прошла успешно!", {
+                    position: "bottom-right",
+                    timeout: 2970,
+                    closeOnClick: true,
+                    pauseOnFocusLoss: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    draggablePercent: 0.6,
+                    showCloseButtonOnHover: true,
+                    hideProgressBar: true,
+                    closeButton: "button",
+                    icon: true,
+                    rtl: false
+                });
+            }
+        },
+        beforeCreate(){
+            if (this.event.award){
+                let pointerBoxLink = document.createElement("link");
+                pointerBoxLink.setAttribute("rel", "stylesheet");
+                pointerBoxLink.setAttribute("href", "/pointer.css");
+                pointerBoxLink.setAttribute("id", "event-pointer-style");
+                let docHead = document.querySelector("head");
+                docHead.append(pointerBoxLink);
+            }
+        },
+        beforeUnmount(){
+            try {
+                let pointerBoxLink = document.querySelector("#event-pointer-style");
+                let parentNode = pointerBoxLink.parentNode;                
+                parentNode.removeChild(pointerBoxLink);
+            }catch(e){}
         }
     }
 </script>
 
 <style scoped>
+    .btn-danger {
+        background-color: #dc3545;
+    }
+    .btn-success {
+        background-color: #4caf50;
+    }
     .news-post img{
         width: 100%;
     }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+
+const app = createApp(App)
+
+app.use(Toast, {
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 20,
+    newestOnTop: true
+  });
+app.use(router)
+app.mount('#app')
