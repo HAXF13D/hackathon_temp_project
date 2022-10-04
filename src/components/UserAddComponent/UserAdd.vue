21611c35f5bd9f601e8e580f16edc1b20202a4bd
HAXF13D <vovik0312@gmail.com>
2022-10-04 20:45:25 +0300

    finish form

--- src/components/UserAddComponent/UserAdd.vue
+++ src/components/UserAddComponent/UserAdd.vue
@@ -5,33 +5,84 @@
         <div class="row mt-sm-5">
             <div class="col-xl-3"></div>
             <div class="col-xl-6 mt-5 user-add-border">
-                <form class="row g-3">
+                <form class="row g-3" @submit.prevent="checkForm">
                     <div class="col-md-6 pe-md-3 ps-md-4">
                         <label for="inputName" class="form-label label-text mt-4">Имя</label>
-                        <input type="text" class="form-control input-form text-start py-2 me-3 pe-0" id="inputName" placeholder="Иван">
+                        <input 
+                            required type="text" 
+                            class="form-control input-form text-start py-2 me-3 pe-0" 
+                            id="inputName" 
+                            placeholder="Иван"
+                            name="inputName"
+                        >
                     </div>
                     <div class="col-md-6 ps-md-3 pe-md-4">
                         <label for="inputSurname" class="form-label label-text mt-4">Фамилия</label>
-                        <input type="text" class="form-control input-form text-start py-2 me-3 pe-0" id="inputSurname" placeholder="Иванов">
+                        <input 
+                            required type="text" 
+                            class="form-control input-form text-start py-2 me-3 pe-0" 
+                            id="inputSurname" 
+                            placeholder="Иванов"
+                            name="inputSurname"
+                        >
                     </div>
                     <div class="col-md-6 pe-md-3 ps-md-4 mt-0">
                         <label for="inputBirthYear" class="form-label label-text mt-4">Дата рождения</label>
-                        <input type="date" class="form-control input-form text-start py-2 me-3 pe-0" id="inputBirthYear">
+                        <input 
+                            required type="date" 
+                            class="form-control input-form text-start py-2 me-3 pe-0" 
+                            id="inputBirthYear"
+                            name="inputBirthYear"
+                        >
                     </div>
                     <div class="col-md-6 ps-md-3 pe-md-4 mt-0">
-                        <label for="inputSex" class="form-label label-text mt-4">Пол</label>
-                        <input type="email" class="form-control input-form text-start py-2 me-3 pe-0" id="inputSex">
+                        <label for="inputSex" class="form-label label-text mt-4 d-flex">Пол</label>
+                        <div class="d-flex justify-content-start mt-3">
+                            <div class="form-check form-check-inline me-md-2">
+                                <input 
+                                    required class="form-check-input" 
+                                    type="radio" 
+                                    name="inlineRadioOptions" 
+                                    id="inputSex1" 
+                                    value="male"
+                                >
+                                <label class="form-check-label sex-checkbox" for="inlineRadio1">Мужской</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input 
+                                    required class="form-check-input" 
+                                    type="radio" 
+                                    name="inlineRadioOptions" 
+                                    id="inputSex2" 
+                                    value="famale"
+                                >
+                                <label class="form-check-label sex-checkbox" for="inlineRadio2">Женский</label>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-md-6 pe-md-3 ps-md-4 mt-0">
                         <label for="inputEmail" class="form-label label-text mt-4">Эл. адрес</label>
-                        <input type="email" class="form-control input-form text-start py-2 me-3 pe-0" id="inputEmail" placeholder="example@mail.com">
+                        <input 
+                            type="email" 
+                            class="form-control input-form text-start py-2 me-3 pe-0" 
+                            id="inputEmail" 
+                            name="inputEmail"
+                            placeholder="example@mail.com"
+                        >
                     </div>
                     <div class="col-md-6 ps-md-3 pe-md-4 mt-0">
                         <label for="inputTelephoneNumber" class="form-label label-text mt-4">Номер телефона</label>
-                        <input type="tel" class="form-control input-form text-start py-2 me-3 pe-0" id="inputTelephoneNumber" pattern="^\8\d{10}$" placeholder="81234567890">
+                        <input 
+                            type="tel" 
+                            class="form-control input-form text-start py-2 me-3 pe-0" 
+                            id="inputTelephoneNumber"
+                            name="inputTelephoneNumber" 
+                            pattern="^\8\d{10}$" 
+                            placeholder="81234567890"
+                        >
                     </div>
                     <div class="d-grid col-12 mx-auto px-md-4 py-4">
-                        <button type="submit" class="btn btn-primary">Сгенерировать пароль</button>
+                        <button class="btn btn-primary">Сгенерировать пароль</button>
                     </div>
                 </form>
             </div>
@@ -48,11 +99,29 @@
     export default{
         name: 'useradd',
         data(){
-
+            return {
+                form: {
+                    inputName: '',
+                    inputSurname: '',
+                    inputBirthYear: '',
+                    inlineRadioOptions: '',
+                    inputEmail: '',
+                    inputTelephoneNumber: ''
+                }
+            }
         },
         components: {
             CustomHeader
         },
+        methods:{
+            addUser(event){
+                event.preventDefault();
+                console.log(this.inputName);
+            },
+            checkForm(event){
+                console.log("JOPA")
+            }
+        }
     }
 </script>
 
@@ -99,5 +168,10 @@
     input[type="date"]::-webkit-inner-spin-button,
     input[type="date"]::-webkit-calendar-picker-indicator {
         padding-right: 5%;
+    }
+    .sex-checkbox{
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 18px;
     }
 </style>
